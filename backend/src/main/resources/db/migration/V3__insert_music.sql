c7dac1ff4c113b295049d39e70445dcae67fb0ba
nruir <iinniuu@naver.com>
2026-01-15 20:01:41 +0900

    add db music

---
+++ backend/src/main/resources/db/migration/V3__insert_music.sql
@@ -0,0 +1,37 @@
+-- V3__insert_music.sql
+
+INSERT INTO music
+(title, artist, file_path, bpm, duration, difficulty)
+VALUES
+(
+  'Birthday Star',
+  'soyun',
+  '../static/music/birthday_star.mp3',
+  90,
+  104,
+  2
+),
+(
+  'Combo',
+  'gongju',
+  '../static/music/combo.mp3',
+  88,
+  60,
+  1
+),
+(
+  'Rock',
+  'onepearl',
+  '../static/music/rock.mp3',
+  116,
+  65,
+  2
+),
+(
+  'Sungsimdang',
+  'jjooya',
+  '../static/music/sungsimdang.mp3',
+  130,
+  69,
+  3
+);
