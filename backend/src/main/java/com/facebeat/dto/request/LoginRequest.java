ed605880012366a156d61d36db5bacd57a6258ae
soyun11 <567654@o.cnu.ac.kr>
2026-01-18 20:04:25 +0900

    feat: 비밀번호 암호화(BCrypt) 설정 및 회원가입/로그인 서비스 로직 구현
    - Config: SecurityConfig 생성 및 BCryptPasswordEncoder 빈 등록
    - Service: UserService에 암호화(encode) 및 비밀번호 대조(matches) 로직 적용
    - Fix: User 엔티티 및 DTO(Login/Signup)에 Getter 어노테이션 추가

--- backend/src/main/java/com/facebeat/dto/request/LoginRequest.java
+++ backend/src/main/java/com/facebeat/dto/request/LoginRequest.java
@@ -1,10 +1,13 @@
 package com.facebeat.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
-
-    public LoginRequest() {}
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
